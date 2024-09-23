15e332501b3ac5502875330622fbf00beee51895
Vishnyakov06 <147653545+Vishnyakov06@users.noreply.github.com>
2024-09-23 11:20:08 +0300

    Task8

---
+++ Task8/Task8/Program.cs
@@ -0,0 +1,54 @@
+﻿using MyVector;
+public class Project
+{
+    public class MyStack<T> : MyVector<T>
+    {
+        private MyVector<T> stack;
+        private int top;
+        public MyStack(){
+            top = -1;
+            stack = new MyVector<T>(1);
+        }
+        public void Push(T item)
+        {
+            top++;
+            stack.Add(item);
+        }
+        public void Pop() { 
+            stack.Remove(top);
+            top--;
+        }
+        public T Peek()
+        {
+            if(top==-1) throw new Exception("Stack is empty");
+            else return stack.Get(top);
+        }
+        public bool Empty()
+        {
+            return stack.Count == 0;
+        }
+        public int Search(T item) { 
+            if(stack.IndexOf(item) == -1) return -1;
+            return top-stack.IndexOf(item)+1;
+        }
+        public void print()
+        {
+            for(int i=0;i<=top; i++)
+                Console.WriteLine(stack.Get(i));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            Console.WriteLine(stack.Peek());
+            stack.Pop();
+            Console.WriteLine(stack.Search(44));
+        }
+    }
+}
