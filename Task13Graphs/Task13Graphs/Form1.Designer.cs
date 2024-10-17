b10789fea2e7861958388967f95dab931c52aede
Vishnyakov06 <147653545+Vishnyakov06@users.noreply.github.com>
2024-10-17 19:03:21 +0300

    Task13

---
+++ Task13Graphs/Task13Graphs/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Task13Graphs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.Location = new System.Drawing.Point(31, 13);
+            this.zedGraph.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(730, 555);
+            this.zedGraph.TabIndex = 0;
+            this.zedGraph.UseExtendedPrintDialog = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(1059, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сгенерировать массив";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(1059, 456);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(256, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Начать тесты";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1089, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(1089, 125);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(226, 24);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1514, 639);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Task13Graphs/Task13Graphs/Form1.cs
@@ -0,0 +1,240 @@
+﻿using T13;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using System.Runtime;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Collections.Generic;
+using System.Drawing;
+using ZedGraph;
+namespace Task13Graphs
+{
+
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public void InsertPath()
+        {
+            string appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            pathToArray = appDir + @"\arrayy.txt";
+            pathToTime = appDir + @"\timee.txt";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.comboBox1.Items.Add("Первая группа");
+            this.comboBox1.Items.Add("Вторая группа");
+            this.comboBox1.Items.Add("Третья группа");
+            this.comboBox1.SelectedText = "Выберите группу";
+            this.comboBox2.SelectedText = "Выберете тип данных";
+            this.comboBox2.Items.Add("Int");
+            this.comboBox2.Items.Add("Double");
+            this.comboBox2.Items.Add("Char");
+
+        }
+        int flag = 1;
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Sorting sort = new Sorting();
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    flag = 1;
+                    switch (comboBox2.SelectedIndex)
+                    {
+                        case 0:
+                            for (int length = 10; length <= 10000; length *= 10)
+                                sort.SpeedTime(sort.GenerateIntArray, length, sort.BubbleSort, sort.InsertionSort, sort.SelectionSort, sort.ShakerSort, sort.GnomeSort);
+                            break;
+                        case 1:
+                            for (int length = 10; length <= 10000; length *= 10)
+                                sort.SpeedTime(sort.GenerateDoubleArray, length, sort.BubbleSort, sort.InsertionSort, sort.SelectionSort, sort.ShakerSort, sort.GnomeSort);
+                            break;
+                        case 2:
+                            for (int length = 10; length <= 10000; length *= 10)
+                                sort.SpeedTime(sort.GenerateCharArray, length, sort.BubbleSort, sort.InsertionSort, sort.SelectionSort, sort.ShakerSort, sort.GnomeSort);
+                            break;
+                        
+                    }
+                    break;
+                case 1:
+                    flag = 2;
+                    switch (comboBox2.SelectedIndex)
+                    {
+                        case 0:
+                            for (int length = 10; length <= 10000; length *= 10)
+                                sort.SpeedTime(sort.GenerateIntArray, length, sort.ShellSort, sort.BitonicSort, sort.TreeSort);
+
+                            break;
+                        case 1:
+                            for (int length = 10; length <= 10000; length *= 10)
+                                sort.SpeedTime(sort.GenerateDoubleArray, length, sort.ShellSort, sort.BitonicSort, sort.TreeSort);
+                            break;
+                        case 2:
+                            for (int length = 10; length <= 10000; length *= 10)
+                                sort.SpeedTime(sort.GenerateCharArray, length, sort.ShellSort, sort.BitonicSort, sort.TreeSort);
+                            break;
+                        
+                    }
+                    break;
+                case 2:
+                    flag = 3;
+                    switch (comboBox2.SelectedIndex)
+                    {
+                        case 0:
+                            for (int length = 10; length <= 10000; length *= 10)
+                                sort.SpeedTime(sort.GenerateIntArray, length, sort.CombSort,sort.QuickSort, sort.MergeSort);
+                            break;
+                        case 1:
+                            for (int length = 10; length <= 10000; length *= 10)
+                                sort.SpeedTime(sort.GenerateDoubleArray, length, sort.CombSort, sort.QuickSort, sort.MergeSort);
+                            break;
+                        case 2:
+                            for (int length = 10; length <= 10000; length *= 10)
+                                sort.SpeedTime(sort.GenerateCharArray, length, sort.CombSort, sort.QuickSort, sort.MergeSort);
+                            break;
+                       
+                    }
+                    break;
+            }
+        }
+        string pathToArray;
+        string pathToTime;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Sorting sort = new Sorting();
+            InsertPath();
+            List<List<double>> time = new List<List<double>>();
+            try
+            {
+                StreamReader sr = new StreamReader(pathToTime);
+                string line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    List<double> speed = new List<double>();
+                    string[] lineArray = line.Split(' ');
+                    foreach (string str in lineArray)
+                    {
+                        speed.Add(Convert.ToDouble(str));
+                    }
+                    time.Add(speed);
+                    line = sr.ReadLine();
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex); }
+            GraphPane pane = zedGraph.GraphPane;
+            pane.CurveList.Clear();
+            pane.XAxis.Scale.Max = 10000;
+            pane.XAxis.Title.Text = "Размер массива";
+            pane.YAxis.Title.Text = "Время выполнения";
+            pane.Title.Text = "Исследование скорости работы сортировок";
+            for (int i = 0; i < time[0].Count; i++)
+            {
+                PointPairList pointList = new PointPairList();
+                int x = 10;
+
+                for (int j = 0; j < time.Count; j++)
+                {
+
+                    pointList.Add(x, time[j][i]);
+                    x *= 10;
+                }
+                switch (i)
+                {
+                    case 0:
+                        if (flag == 1)
+                        {
+                            pane.AddCurve("BubbleSort: " + i, pointList, Color.Black, SymbolType.Default);
+                        }
+                        if (flag == 2)
+                        {
+                            pane.AddCurve("ShellSort: " + i, pointList, Color.Black, SymbolType.Default);
+                        }
+                        if (flag == 3)
+                        {
+                            pane.AddCurve("CombSort: " + i, pointList, Color.Black, SymbolType.Default);
+                        }
+                        break;
+                    case 1:
+                        if (flag == 1)
+                        {
+                            pane.AddCurve("InsertSort: " + i, pointList, Color.Red, SymbolType.Default);
+                        }
+                        if (flag == 2)
+                        {
+                            pane.AddCurve("TreeSort: " + i, pointList, Color.Black, SymbolType.Default);
+                        }
+                        if (flag == 3)
+                        {
+                            pane.AddCurve("HeapSort: " + i, pointList, Color.Black, SymbolType.Default);
+                        }
+                        break;
+                    case 2:
+                        if (flag == 1)
+                        {
+                            pane.AddCurve("SelectionSort: " + i, pointList, Color.Pink, SymbolType.Default);
+                        }
+                        if (flag == 2)
+                        {
+                            pane.AddCurve("BitonicSort: " + i, pointList, Color.Black, SymbolType.Default);
+                        }
+                        if (flag == 3)
+                        {
+                            pane.AddCurve("QuickSort: " + i, pointList, Color.Black, SymbolType.Default);
+                        }
+                        break;
+                    case 3:
+                        if (flag == 1)
+                        {
+                            pane.AddCurve("ShakerSort: " + i, pointList, Color.Yellow, SymbolType.Default);
+                        }
+                        if (flag == 3)
+                        {
+                            pane.AddCurve("MergeSort: " + i, pointList, Color.Black, SymbolType.Default);
+                        }
+                        break;
+                    case 4:
+                        if (flag == 1)
+                        {
+                            pane.AddCurve("GnomeSort: " + i, pointList, Color.Green, SymbolType.Default);
+                        }
+                        if (flag == 3)
+                        {
+                            pane.AddCurve("CountingSort: " + i, pointList, Color.Black, SymbolType.Default);
+                        }
+                        break;
+                    case 5:
+                        pane.AddCurve("RadixSort: " + i, pointList, Color.Aquamarine, SymbolType.Default);
+                        break;
+                }
+            }
+
+            zedGraph.AxisChange();
+            zedGraph.Invalidate();
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
