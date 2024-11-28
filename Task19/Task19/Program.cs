33f620971f253793b5dadf371d530dfd23fec092
Vishnyakov06 <147653545+Vishnyakov06@users.noreply.github.com>
2024-11-28 21:18:40 +0300

    Task19

---
+++ Task19/Task19/Program.cs
@@ -0,0 +1,32 @@
+﻿using Task18;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+public class HTML
+{
+    static void Main(string[] args)
+    {
+        MyHashMap<string, int> teg = new MyHashMap<string, int>();
+        string path = "input.txt";
+        StreamReader sr = new StreamReader(path);
+        string? line = sr.ReadLine();
+        string pattern = @"(?<=</?)([a-zA-Z][\w-]*)(?=/?>)";
+
+        if (line == null)Console.WriteLine("Строчка пуста");
+        while(line != null)
+        {
+            MatchCollection matches = Regex.Matches(line, pattern);
+            foreach (Match match in matches) {
+                if (!teg.ContainsKey(match.Value.ToLower()))
+                    teg.Put(match.Value.ToLower(), 1);
+                else
+                    teg.Put(match.Value.ToLower(),teg.Get(match.Value.ToLower())+1);
+            }
+            
+            line = sr.ReadLine();
+        }
+        sr.Close();
+        IEnumerable<KeyValuePair<string, int>> pairs = teg.EntrySet();
+        foreach (var pair in pairs)
+            Console.WriteLine($"Ключ: {pair.Key}, Значение: {pair.Value}");
+    }
+}
